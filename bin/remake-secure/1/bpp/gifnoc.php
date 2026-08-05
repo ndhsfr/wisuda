e2a709089ac8153aaa027b9b00a49fdaf83fcf45
ndhsfr <indahsafitri@unimugo.ac.id>
2026-08-05 12:06:45 +0700

    Delete bin directory

--- bin/remake-secure/1/bpp/gifnoc.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$_raid = array(
-	'institution'=>'Universitas Muhammadiyah Gombong',
-	'url'=>'https://daftarwisuda.unimugo.ac.id',
-	'theme'=>'unimugo',
-);
-?>
--- bin/remake-secure/1/gifnoc.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$_raid = array(
-	'institution'=>'Universitas Muhammadiyah Gombong',
-	'url'=>'https://wisuda.unimugo.ac.id',
-	'theme'=>'unimugo',
-);
-?>
